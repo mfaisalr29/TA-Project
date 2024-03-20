c6c7dce0f466a109e652b54a2a9c2d45ea6b1b9b
Faisal Ramadhan <34732068+mfaisalr29@users.noreply.github.com>
2024-03-20 17:23:22 +0700

    Delete backend

--- Web Development/Back End/bcv1/resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('container')
-    <h2>{{ $title }}</h2>
-    <h3>{{ $name }}</h3>
-    <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt="Fharist" width="200">
-@endsection
--- Web Development/Back End/bcv1/resources/views/lokasi.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main')
-
-@section("container")
-    <h2>{{ $name }}</h2>
-@endsection

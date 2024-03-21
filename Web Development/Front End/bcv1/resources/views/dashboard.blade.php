3c4869925558d9cb7f11e561c3a4cca9415eb752
Ristess <130834582+Ristess@users.noreply.github.com>
2024-03-21 15:55:15 +0700

    Update dashboard.blade.php
    Update

--- Web Development/Front End/bcv1/resources/views/dashboard.blade.php
+++ Web Development/Front End/bcv1/resources/views/dashboard.blade.php
@@ -20,15 +20,15 @@
             <div class="col-md-3">
                 <div class="p-3 mb-2" style="background-color: #394E69; border-radius: 10px;">
                     <div class="d-flex align-items-center">
-                        <img src="{{ asset('img/Profile.png') }}" class="img-fluid mr-2" style="max-height : 100px; border-radius: 40px;">
-                        <h5 class="mb-0 text-white">Nomor Rumah</h5>
+                        <img src="{{ asset('img/Profile.png') }}" class="img-fluid mr-2" style="max-height : 100px; border-radius: 40px; padding : 10px">
+                        <h5 class="mb-0 text-black">Nomor Rumah</h5>
                     </div>
                     <hr style="border-top: 1px solid #000000;">
                     <div class="p-2 mb-2" style="background-color: #394E69">
-                        <p class="mb-0 text-white">Detail Tagihan IPL</p>
+                        <p class="mb-0 text-black">Detail Tagihan IPL</p>
                     </div>
                     <div class="p-2 mb-2" style="background-color: #394E69">
-                        <p class="mb-0 text-white">Profile</p>
+                        <p class="mb-0 text-black">Profile</p>
                     </div>
                 </div>
             </div>

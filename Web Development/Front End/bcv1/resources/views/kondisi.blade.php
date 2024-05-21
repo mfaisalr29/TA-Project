781fe2f5d6a737236476c35ba1b7f1bf2371885a
Ristess <130834582+Ristess@users.noreply.github.com>
2024-05-21 18:21:35 +0700

    Firebase Realtime database
    Konek Firebase sudah bisa

--- Web Development/Front End/bcv1/resources/views/kondisi.blade.php
+++ Web Development/Front End/bcv1/resources/views/kondisi.blade.php
@@ -47,7 +47,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('mode-kontrol')">
+                                    <div class="interactive-circle" id="mode-kontrol" onclick="handleClick('mode-kontrol', 'ControlSystem/Automation', 'gambar1', '{{ asset('img/lightbulb-on.png') }}', '{{ asset('img/lightbulb-off.png') }}')">
                                         <img id="gambar1" src="{{ asset('img/lightbulb-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
                                         <h5 class="mt-2">Mode Kontrol</h5>
                                     </div>
@@ -55,17 +55,17 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('bor-besar')">
+                                    <div class="interactive-circle" id="bor-besar" onclick="handleClick('bor-besar', 'ControlSystem/Reservoir2/Relay1', 'gambar2', '{{ asset('img/lightbulb-on.png') }}', '{{ asset('img/lightbulb-off.png') }}')">
                                         <img id="gambar2" src="{{ asset('img/lightbulb-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
-                                        <h5 class="mt-2">Bor Besar</h5>
+                                        <h5 class="mt-2">Pompa Celup Besar</h5>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('kondisi-air')">
+                                    <div class="interactive-circle" id="kondisi-air" onclick="handleClick('kondisi-air', 'ControlSystem/Reservoir2/Relay2', 'gambar3', '{{ asset('img/lightbulb-on.png') }}', '{{ asset('img/lightbulb-off.png') }}')">
                                         <img id="gambar3" src="{{ asset('img/lightbulb-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
-                                        <h5 class="mt-2">Kondisi Air</h5>
+                                        <h5 class="mt-2">Pompa Celup Kecil</h5>
                                     </div>
                                 </div>
                             </div>
@@ -76,7 +76,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('pompa-dorong')">
+                                    <div class="interactive-circle" id="pompa-dorong" onclick="handleClick('pompa-dorong', 'ControlSystem/Reservoir2/Relay3', 'gambar4', '{{ asset('img/lightbulb-on.png') }}', '{{ asset('img/lightbulb-off.png') }}')">
                                         <img id="gambar4" src="{{ asset('img/lightbulb-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
                                         <h5 class="mt-2">Pompa Dorong</h5>
                                     </div>
@@ -84,7 +84,7 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('reservoir-atas')">
+                                    <div class="interactive-circle" id="reservoir-atas" onclick="handleClick('reservoir-atas', 'ControlSystem/Reservoir1/Radar', 'gambar5', '{{ asset('img/water-pump.png') }}', '{{ asset('img/water-pump-off.png') }}')">
                                         <img id="gambar5" src="{{ asset('img/water-pump-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
                                         <h5 class="mt-2">Reservoir Atas</h5>
                                     </div>
@@ -92,7 +92,7 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="text-center">
-                                    <div class="interactive-circle" onclick="handleClick('reservoir-bawah')">
+                                    <div class="interactive-circle" id="reservoir-bawah" onclick="handleClick('reservoir-bawah', 'ControlSystem/Reservoir2/RadarPompa3', 'gambar6', '{{ asset('img/water-pump.png') }}', '{{ asset('img/water-pump-off.png') }}')">
                                         <img id="gambar6" src="{{ asset('img/water-pump-off.png') }}" class="img-fluid" style="width: 100px; height: 100px;">
                                         <h5 class="mt-2">Reservoir Bawah</h5>
                                     </div>
@@ -104,63 +104,50 @@
             </div>
         </div>
 
-        <script type="module" src="{{ asset('js/firebase.js') }}"></script>
+        <!-- Impor modul Firebase dari CDN -->
         <script type="module">
-            import { database, ref, set, get, child, onValue } from "{{ asset('js/firebase.js') }}";
+            // Impor modul Firebase
+            import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+            import { getDatabase, ref, set, get, child, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
 
-            function handleClick(elementId) {
-                console.log("Element clicked:", elementId); // Debugging log
-                var img;
-                switch (elementId) {
-                    case 'mode-kontrol':
-                        img = document.getElementById('gambar1');
-                        img.src = "{{ asset('img/lightbulb-on.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Automation', 1);
-                        break;
-                    case 'bor-besar':
-                        img = document.getElementById('gambar2');
-                        img.src = "{{ asset('img/lightbulb-on.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Reservoir2/RadarBorBesar1', 1);
-                        break;
-                    case 'kondisi-air':
-                        img = document.getElementById('gambar3');
-                        img.src = "{{ asset('img/lightbulb-on.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Reservoir1/Radar', 1);
-                        break;
-                    case 'pompa-dorong':
-                        img = document.getElementById('gambar4');
-                        img.src = "{{ asset('img/lightbulb-on.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Reservoir2/Pompa3', 1);
-                        break;
-                    case 'reservoir-atas':
-                        img = document.getElementById('gambar5');
-                        img.src = "{{ asset('img/water-pump.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Reservoir1/Radar', 1);
-                        break;
-                    case 'reservoir-bawah':
-                        img = document.getElementById('gambar6');
-                        img.src = "{{ asset('img/water-pump.png') }}";
-                        changeCircleColor(elementId, '#0000FF');
-                        updateFirebase('Reservoir2/RadarPompa3', 1);
-                        break;
-                    default:
-                        break;
+            const firebaseConfig = {
+                apiKey: "AIzaSyBrFK8HL0bBK7QaVm5dsQJ9Gk9Nm5-LmlU",
+                authDomain: "bcv1-f450b.firebaseapp.com",
+                databaseURL: "https://bcv1-f450b-default-rtdb.asia-southeast1.firebasedatabase.app",
+                projectId: "bcv1-f450b",
+                storageBucket: "bcv1-f450b.appspot.com",
+                messagingSenderId: "632085793199",
+                appId: "1:632085793199:web:64563abd2d0d8faad2c75a",
+            };
+
+            // Inisialisasi Firebase
+            const app = initializeApp(firebaseConfig);
+            const database = getDatabase(app);
+
+            // Definisikan fungsi global
+            window.handleClick = function(elementId, firebasePath, imageId, onImage, offImage) {
+                const img = document.getElementById(imageId);
+                const currentSrc = img.src;
+                const newValue = currentSrc.includes('off') ? 1 : 0;
+                const newImage = newValue === 1 ? onImage : offImage;
+
+                img.src = newImage;
+                updateFirebase(firebasePath, newValue);
+                // changeCircleColor(elementId, newValue === 1 ? 'yellow' : 'transparent'); 
+            };
+
+            function changeCircleColor(elementId, color) {
+                const circle = document.getElementById(elementId);
+                if(circle) {
+                    circle.style.backgroundColor = color;
+                } else {
+                    console.error("Element with ID " + elementId + " not found.");
                 }
             }
 
-            function changeCircleColor(elementId, color) {
-                var circle = document.getElementById(elementId);
-                circle.style.backgroundColor = color;
-            }
-
             function updateFirebase(path, value) {
-                const dbRef = ref(database, `ControlSystem/${path}`);
-                console.log(`Updating Firebase at ${path} with value ${value}`);
+                const dbRef = ref(database, path);
+                console.log(`Updating Firebase at ${path} with value ${value}`); 
                 set(dbRef, value)
                     .then(() => {
                         console.log("Data updated successfully!");
@@ -170,34 +157,33 @@
                     });
             }
 
-            // Example of reading data from Firebase
             function readFirebase(path) {
                 const dbRef = ref(database);
-                get(child(dbRef, `ControlSystem/${path}`))
+                return get(child(dbRef, path))
                     .then((snapshot) => {
                         if (snapshot.exists()) {
-                            console.log(snapshot.val());
+                            return snapshot.val();
                         } else {
                             console.log("No data available");
+                            return 0; 
                         }
                     })
                     .catch((error) => {
                         console.error("Error reading data: ", error);
+                        return 0; 
                     });
             }
 
-            // Example of listening to data changes from Firebase
             function listenFirebase(path) {
-                const dbRef = ref(database, `ControlSystem/${path}`);
+                const dbRef = ref(database, path);
                 onValue(dbRef, (snapshot) => {
                     const data = snapshot.val();
                     console.log(data);
                 });
             }
 
-            // Initialize listeners
-            listenFirebase('Reservoir1/Radar');
-            listenFirebase('Reservoir2/RadarBorBesar1');
+            listenFirebase('ControlSystem/Reservoir1/Radar');
+            listenFirebase('ControlSystem/Reservoir2/RadarBorBesar1');
         </script>
     </div>
 </div>
--- Web Development/Front End/bcv1/resources/views/tagihan.blade.php
+++ Web Development/Front End/bcv1/resources/views/tagihan.blade.php
@@ -31,7 +31,7 @@
                     <div class="p-2 mb-2">
                         <nav class="nav flex-column">
                             <a class="nav-link {{ ($title === "Dashboard Admin") ? 'active' : ''}}" href="/dashboardadmin">Dashboard</a>
-                            <a class="nav-link {{ ($title === "Tagihan IPL Admin") ? 'active' : ''}}" href="/tagihanipladmin">Tagihan IPL</a>
+                            <a class="nav-link {{ ($title === "Input Tagihan") ? 'active' : ''}}" href="/tagihanipladmin">Tagihan IPL</a>
                             <a class="nav-link {{ ($title === "Kondisi Air dan Alat") ? 'active' : ''}}" href="/kondisi">Kondisi Air dan Alat</a>
                             <a class="nav-link {{ ($title === "Daftar Akun Warga") ? 'active' : ''}}" href="#">Daftar Akun Warga</a>
                             <a class="nav-link {{ ($title === "Profile Admin") ? 'active' : ''}}" href="/profileadmin">Profile</a>

e746d4ca19ea153547f3a4d364e83dc9ba3be883
Ristess <130834582+Ristess@users.noreply.github.com>
2024-06-17 15:24:22 +0700

    Update sanctum.php

--- Web Development/Front End/bcv1/config/sanctum.php
+++ Web Development/Front End/bcv1/config/sanctum.php
@@ -6,7 +6,7 @@ return [
 
 
 
-'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,34.101.172.8')),
+'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,34.101.66.3')),
 
 
     'guard' => ['web'],

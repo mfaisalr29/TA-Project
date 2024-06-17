cd2ecdf7bbf58f20111871a1c4b106d674a7b987
Ristess <130834582+Ristess@users.noreply.github.com>
2024-06-18 01:23:45 +0700

    Update untuk ke GCP
    Agar website di GCP berfungsi dengan baik

--- Web Development/Front End/bcv1/config/sanctum.php
+++ Web Development/Front End/bcv1/config/sanctum.php
@@ -6,7 +6,10 @@ return [
 
 
 
-'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,34.101.66.3')),
+'stateful' => explode(',', env(
+    'SANCTUM_STATEFUL_DOMAINS',
+    'localhost,127.0.0.1,34.101.66.3:8000'
+)),
 
 
     'guard' => ['web'],

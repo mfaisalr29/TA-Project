a121a2959cc587b005f5c4b69399aae286d84cfa
Faisal Ramadhan <34732068+mfaisalr29@users.noreply.github.com>
2024-06-18 21:13:46 +0700

    Menambahkan icon pada browser

--- Web Development/Front End/bcv1/resources/views/layouts/main.blade.php
+++ Web Development/Front End/bcv1/resources/views/layouts/main.blade.php
@@ -11,6 +11,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
   </head>
   <body>
     @if ($title !== "Login")
